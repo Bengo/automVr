5b5109f2ccbc4eded7430f11bfeac636ceef068d
Bengo <bengo@github.com>
2014-10-04 11:41:31 +0200

    Debug appel scipts

--- site/automVr.php
+++ site/automVr.php
@@ -2,19 +2,22 @@
 header('Content-Type: text/xml; charset=UTF-8');
 
 if(isset($_GET['zone']) AND isset($_GET['action'])){
-{
 	$action = escapeshellarg($_GET['action']);
 	$zone = escapeshellarg($_GET['zone']);
+	echo('../scripts/volets.pl '.$action.' '.$zone);
 	exec('../scripts/volets.pl '.$action.' '.$zone);
+	
 
-} else if(isset($_GET['option']) AND isset($_GET['value'])){
+} elseif(isset($_GET['option']) AND isset($_GET['value'])){
 	$option = escapeshellarg($_GET['option']);
 	$value = escapeshellarg($_GET['value']);
 	if(strcmp($option, "modeFete")){
 		if(strcmp($value, "on")){
+			echo('../scripts/volets.pl option modeFeteOn');
 			exec('../scripts/volets.pl option modeFeteOn');
 		}
 		if(strcmp($value, "off")){
+			echo('../scripts/volets.pl option modeFeteOff');
 			exec('../scripts/volets.pl option modeFeteOff');
 		}
 		
