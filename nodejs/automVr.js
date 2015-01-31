0885d4435c00ff3e8671d59209326fc56733a7d9
Bengo <bengo@github.com>
2015-01-31 15:24:55 +0100

    Mise en place de la gestion du mode fete

--- nodejs/automVr.js
+++ nodejs/automVr.js
@@ -5,21 +5,36 @@ var exec = require('child_process').exec;
 
 
 var app = express();
-
 var static = express();
+
 //tout le repertoire public est servi en static
 static.use(express.static(__dirname + '/public'));
 var api = express();
 var config = ini.parse(fs.readFileSync('../config.ini', 'utf-8'))
 
 function agir_volets(zone,position){
-	var exec_volets = exec('../scripts/volets.pl '+zone+' '+position,
-							function (error, stdout, stderr) {});
+	exec('../scripts/volets.pl '+zone+' '+position,	function (error, stdout, stderr) {});
+}
+
+function mode_fete_actif(actif, mode_fete_result){
+	if(JSON.parse(actif)) {
+		exec('../scripts/volets.pl option modeFeteOn',	function (error, stdout, stderr) {});
+		return "actif";
+	} else {
+		exec('../scripts/volets.pl option modeFeteOff',	function (error, stdout, stderr) {});
+		return "inactif";
+	}
 }
 
 api.get('/options', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.end(JSON.stringify(config));
+});
+
+api.get('/options/modefe/:actif', function(req, res) {
+	var mode_fete_result=mode_fete_actif(req.params.actif, mode_fete_result);
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("mode fete mis a "+mode_fete_result);
 });
 
 api.get('/volets/:zone/:position', function(req, res) {
