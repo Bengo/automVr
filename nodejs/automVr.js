0b8b6d786ff7fb7265615342ead1a1bd1eef9632
Bengo <bengo@github.com>
2015-01-31 14:42:13 +0100

    Mise en place d'une version tournant sous nodeJs

---
+++ nodejs/automVr.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var fs = require('fs');
+var ini = require('ini');
+var exec = require('child_process').exec;
+
+
+var app = express();
+
+var static = express();
+//tout le repertoire public est servi en static
+static.use(express.static(__dirname + '/public'));
+var api = express();
+var config = ini.parse(fs.readFileSync('../config.ini', 'utf-8'))
+
+function agir_volets(zone,position){
+	var exec_volets = exec('../scripts/volets.pl '+zone+' '+position,
+	  function (error, stdout, stderr) {
+		console.log('stdout: ' + stdout);
+		console.log('stderr: ' + stderr);
+		if (error !== null) {
+		  console.log('exec error: ' + error);
+		}
+	});
+}
+
+api.get('/options', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(JSON.stringify(config));
+});
+
+api.get('/volets/:zone/:position', function(req, res) {
+	agir_volets(req.params.zone,req.params.position);
+});
+
+app.use('/api',api);
+app.use('/',static);
+
+app.listen(8080);
